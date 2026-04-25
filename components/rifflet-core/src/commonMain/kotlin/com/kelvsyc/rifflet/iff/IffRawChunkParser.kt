16bed5eaa7a52604d6c0f3ee4f60a3df951104b9
Kelvin Chung <kelv@kelvSYC.com>
2026-04-25 09:21:09 -0700

    Fix potential partial read issue in RawChunkParser

--- components/rifflet-core/src/commonMain/kotlin/com/kelvsyc/rifflet/iff/IffRawChunkParser.kt
+++ components/rifflet-core/src/commonMain/kotlin/com/kelvsyc/rifflet/iff/IffRawChunkParser.kt
@@ -3,7 +3,6 @@ package com.kelvsyc.rifflet.iff
 import com.kelvsyc.rifflet.core.RawChunk
 import com.kelvsyc.rifflet.core.RawChunkParser
 import com.kelvsyc.rifflet.core.readChunkId
-import okio.Buffer
 import okio.Source
 import okio.buffer
 
@@ -15,19 +14,13 @@ object IffRawChunkParser : RawChunkParser {
         val input = source.buffer()
         val type = input.readChunkId()
         val size = input.readInt()
-        val data = Buffer()
-        val bytesRead = input.read(data, size.toLong())
-        if (bytesRead != size.toLong()) {
-            if (bytesRead == -1L) {
-                throw IllegalStateException("Source is exhausted: expected $size bytes")
-            } else {
-                throw IllegalStateException("Unexpected end of source: could only read $bytesRead of $size bytes")
-            }
-        }
+        // readByteString throws EOFException if the source is exhausted before size bytes are read,
+        // correctly handling both total exhaustion and partial reads.
+        val data = input.readByteString(size.toLong())
 
         // IFF requires that chunks with odd lengths be padded with a pad byte, so we consume it
         if (size and 1 != 0) input.skip(1)
 
-        return RawChunk(type, data.readByteString())
+        return RawChunk(type, data)
     }
 }
