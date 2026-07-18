c2ad56b463d18f25f218c7b04635a42d12ceb1b3
Kelvin Chung <kelv@kelvSYC.com>
2026-07-17 23:34:12 -0700

    Fix CTZN parser to defensively handle vanilla/PTW files (missing trailing fields)

--- components/rifflet-civ3/src/commonTest/kotlin/com/kelvsyc/rifflet/internal/civ3/CtznEntryParserTest.kt
+++ components/rifflet-civ3/src/commonTest/kotlin/com/kelvsyc/rifflet/internal/civ3/CtznEntryParserTest.kt
@@ -1,6 +1,7 @@
 package com.kelvsyc.rifflet.internal.civ3
 
 import com.kelvsyc.rifflet.civ3.CtznEntry
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import okio.Buffer
@@ -24,6 +25,7 @@ private fun ctznItemBinary(
     taxes: Int = 0,
     corruption: Int = 0,
     construction: Int = 0,
+    includeTrailingFields: Boolean = true,
 ): Buffer = Buffer().apply {
     writeIntLe(defaultCitizen)
     writePaddedField(singularName, 32)
@@ -33,8 +35,10 @@ private fun ctznItemBinary(
     writeIntLe(luxuries)
     writeIntLe(research)
     writeIntLe(taxes)
-    writeIntLe(corruption)
-    writeIntLe(construction)
+    if (includeTrailingFields) {
+        writeIntLe(corruption)
+        writeIntLe(construction)
+    }
 }
 
 class CtznEntryParserTest : FunSpec({
@@ -53,5 +57,11 @@ class CtznEntryParserTest : FunSpec({
             corruption = 0,
             construction = 0,
         )
+    }
+
+    test("vanilla/PTW-length item (116 bytes, trailing fields absent) defaults them to zero") {
+        val entry = CtznEntryParser.parse(ctznItemBinary(includeTrailingFields = false))
+        entry.corruption shouldBe 0
+        entry.construction shouldBe 0
     }
 })
