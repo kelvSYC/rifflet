2d8ba9bfe027981ab1c5a7cbc1c5a94733a50316
Kelvin Chung <kelv@kelvSYC.com>
2026-07-19 17:00:30 -0700

    Retrofit EspnEntry bit-flag accessors to extensionBitFlag

--- components/rifflet-civ3/src/commonMain/kotlin/com/kelvsyc/rifflet/civ3/EspnEntryFlags.kt
+++ components/rifflet-civ3/src/commonMain/kotlin/com/kelvsyc/rifflet/civ3/EspnEntryFlags.kt
@@ -1,8 +1,12 @@
 package com.kelvsyc.rifflet.civ3
 
+import com.kelvsyc.kotlin.core.traits.integral.BitCollection
+import com.kelvsyc.kotlin.core.traits.integral.extensionBitFlag
+import com.kelvsyc.kotlin.core.traits.integral.int
+
 /**
  * Named accessors for [EspnEntry.missionFlags]'s 2 documented bits, per Apolyton's "Civilization
  * III BIC file format (2nd thread)".
  */
-val EspnEntry.diplomat: Boolean get() = missionFlags and (1 shl 0) != 0
-val EspnEntry.spy: Boolean get() = missionFlags and (1 shl 1) != 0
+val EspnEntry.diplomat: Boolean by BitCollection.int.extensionBitFlag({ missionFlags }, 0)
+val EspnEntry.spy: Boolean by BitCollection.int.extensionBitFlag({ missionFlags }, 1)
